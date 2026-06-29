1bc5c3dc021593ea1b2b8466e2ba60fe8fd5f9ce
Tomoeee889 <ivanaristov889@gmail.com>
2026-06-29 19:34:47 +0300

    Update split_dataset.py

--- mlp_model/split_dataset.py
+++ mlp_model/split_dataset.py
@@ -3,11 +3,10 @@ import torch
 from sklearn.model_selection import train_test_split
 
 PAIRS_PATH = "./train_pairs_clean.pt"
-print("📦 Загружаем чистые пары...")
 data = torch.load(PAIRS_PATH)
 pairs = data["pairs"]
 
-print("🔪 Разделяем: 80% train, 10% val, 10% test")
+print("80% train, 10% val, 10% test")
 train_pairs, temp = train_test_split(pairs, test_size=0.2, random_state=42)
 val_pairs, test_pairs = train_test_split(temp, test_size=0.5, random_state=42)
 
@@ -15,4 +14,4 @@ torch.save({"pairs": train_pairs}, "train_pairs_split.pt")
 torch.save({"pairs": val_pairs}, "val_pairs_split.pt")
 torch.save({"pairs": test_pairs}, "test_pairs_split.pt")
 
-print(f"✅ Train: {len(train_pairs)} | Val: {len(val_pairs)} | Test: {len(test_pairs)}")
+print(f"Train: {len(train_pairs)} | Val: {len(val_pairs)} | Test: {len(test_pairs)}")
